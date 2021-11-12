256b47af11f16232a7d2bf97fd0bd0d5a0218a53
Kirill Kolpakov <kirillkolpakov@MacBook-Pro-Kirill.local>
2021-11-12 16:26:30 +0200

    pravki

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var canvas = new fabric.Canvas('c',{
-    backgroundColor: 'rgb(255,255,255)'
+    backgroundColor: 'rgb(255,255,255)',
+    preserveObjectStacking: true
 });
 
 let ShapeColor = "#000000";
@@ -17,6 +18,8 @@ let rectForName = new fabric.Rect({
 canvas.add(rectForName);
 rectForName.selectable = false;
 
+
+
 let rectForDescription = new fabric.Rect({
     left: 20,
     top: 498,
@@ -31,12 +34,16 @@ let rectForDescription = new fabric.Rect({
 canvas.add(rectForDescription);
 rectForDescription.selectable = false;
 
+
+
 let cardNameCanvas = new fabric.Text('Название карты',
 { 
     left: 35, top: 32,fontFamily: 'Jost',fontSize:24,fontWeight: 'bold',fill: "#111111"
 });
 canvas.add(cardNameCanvas);
 cardNameCanvas.selectable = false;
+
+
 
 let cardDescriptionCanvas = new fabric.Textbox('Описание карты',
 { 
@@ -55,6 +62,8 @@ cardDescriptionCanvas.selectable = false;
 let cardImageSRC;
 let cardBorderImageSRC;
 
+
+
 function loadCardImage(e){
     if (cardImageSRC){
         canvas.remove(cardImageSRC);
@@ -64,21 +73,23 @@ function loadCardImage(e){
         var imgObj = new Image();
         imgObj.src = event.target.result;
         imgObj.onload = function () {
-        cardImageSRC = new fabric.Image(imgObj);
-        cardImageSRC.set({
+            cardImageSRC = new fabric.Image(imgObj);
+            cardImageSRC.set({
                 angle: 0,
                 height:380,
                 width:450,
                 top: 87,
                 left: 20,
-        });
-        canvas.add(cardImageSRC);
-        cardImageSRC.selectable = false;
-        canvas.renderAll();
+            });
+            canvas.add(cardImageSRC);
+            cardImageSRC.selectable = false;
+            canvas.renderAll();
         }
     }
     reader.readAsDataURL(cardImage.files[0]);
 }
+
+
 
 function loadCardBorder(e){
     if (cardBorderImageSRC){
@@ -89,18 +100,18 @@ function loadCardBorder(e){
         var imgObj = new Image();
         imgObj.src = event.target.result;
         imgObj.onload = function () {
-        cardBorderImageSRC = new fabric.Image(imgObj);
-        cardBorderImageSRC.set({
+            cardBorderImageSRC = new fabric.Image(imgObj);
+            cardBorderImageSRC.set({
                 angle: 0,
                 top: 0,
                 left: 0,
-        });
-        cardBorderImageSRC.scaleToHeight(770);
-        cardBorderImageSRC.scaleToWidth(490);
-        canvas.add(cardBorderImageSRC);
-        canvas.sendToBack(cardBorderImageSRC)
-        cardBorderImageSRC.selectable = false;
-        canvas.renderAll();
+            });
+            cardBorderImageSRC.scaleToHeight(770);
+            cardBorderImageSRC.scaleToWidth(490);
+            canvas.add(cardBorderImageSRC);
+            canvas.sendToBack(cardBorderImageSRC)
+            cardBorderImageSRC.selectable = false;
+            canvas.renderAll();
         }
     }
     reader.readAsDataURL(cardBorderImage.files[0]);
@@ -140,6 +151,17 @@ function addCircle(){
     canvas.add(rect);
 }
 
+function addText(){
+    var text = new fabric.IText("Текст",{
+        left: 100,
+        top: 100,
+        fill: ShapeColor,
+        fontFamily: 'Jost',
+        fontSize: 20
+    });
+    canvas.add(text);
+}
+
 function nameChange(){
     cardNameCanvas.set({text:cardName.value});
     canvas.renderAll();
@@ -156,4 +178,37 @@ function downloadCanvas(){
     link.download = 'filename.png';
     link.href = document.getElementById('c').toDataURL()
     link.click();
-  }
+}
+
+function loadImage(){
+    let imageNew;
+    var reader = new FileReader();
+    reader.onload = function (event){
+        var imgObj = new Image();
+        imgObj.src = event.target.result;
+        imgObj.onload = function () {
+            imageNew = new fabric.Image(imgObj);
+            imageNew.set({
+                angle: 0,
+                top: 0,
+                left: 0,
+            });
+            imageNew.scaleToWidth(390);
+            canvas.add(imageNew);
+            canvas.renderAll();
+        }
+    }
+    reader.readAsDataURL(customImage.files[0]);
+}
+
+function sendFront(){
+    console.log("GoForward");
+    canvas.bringForward(canvas.getActiveObject());
+    canvas.renderAll();
+}
+
+function sendBack(){
+    console.log("GoBackward");
+    canvas.sendBackwards(canvas.getActiveObject());
+    canvas.renderAll();
+}
