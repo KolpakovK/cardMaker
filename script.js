2544399f81927840e3af7d132725761f91ca4bdf
Kirill Kolpakov <kirillkolpakov@MacBook-Pro-Kirill.local>
2021-11-14 13:01:53 +0200

    pravki

--- script.js
+++ script.js
@@ -3,17 +3,19 @@ var canvas = new fabric.Canvas('c',{
     preserveObjectStacking: true
 });
 
+canvas.setZoom(0.5)
+
 let ShapeColor = "#000000";
 let rectForName = new fabric.Rect({
-    left: 20,
-    top: 20,
+    left: 40,
+    top: 40,
     fill: "#ffffff",
-    width: 450,
-    height: 50,
-    rx: 16,
-    ry: 16,
+    width: 900,
+    height: 100,
+    rx: 32,
+    ry: 32,
     stroke : 'black',
-    strokeWidth : 2
+    strokeWidth : 4
 });
 canvas.add(rectForName);
 rectForName.selectable = false;
@@ -21,38 +23,75 @@ rectForName.selectable = false;
 
 
 let rectForDescription = new fabric.Rect({
-    left: 20,
-    top: 498,
+    left: 40,
+    top: 996,
     fill: "#ffffff",
-    width: 450,
-    height: 250,
-    rx: 16,
-    ry: 16,
+    width: 900,
+    height: 500,
+    rx: 32,
+    ry: 32,
     stroke : 'black',
-    strokeWidth : 2
+    strokeWidth : 4
 });
 canvas.add(rectForDescription);
 rectForDescription.selectable = false;
 
+let rectForPower = new fabric.Rect({
+    left: canvas.width*2-250,
+    top: canvas.height*2-115,
+    fill: "#ffffff",
+    width: 200,
+    height: 90,
+    rx: 32,
+    ry: 32,
+    stroke : 'black',
+    strokeWidth : 4
+});
+canvas.add(rectForPower);
+rectForPower.selectable = false;
 
 
-let cardNameCanvas = new fabric.Text('Название карты',
+
+let cardNameCanvas = new fabric.IText('Название карты',
 { 
-    left: 35, top: 32,fontFamily: 'Jost',fontSize:24,fontWeight: 'bold',fill: "#111111"
+    left: 70, top: 64,fontFamily: 'Jost',fontSize:48,fontWeight: 'bold',fill: "#111111"
 });
 canvas.add(cardNameCanvas);
 cardNameCanvas.selectable = false;
 
 
+let cardCostCanvas = new fabric.IText('1',
+{ 
+    left: canvas.width*2-96, top: 64,fontFamily: 'Jost',fontSize:48,fontWeight: 'bold',fill: "#111111"
+});
+canvas.add(cardCostCanvas);
+cardCostCanvas.selectable = false;
+
+
+let cardPowerCanvas = new fabric.IText('1/2',
+{ 
+    left: canvas.width*2-150,
+    top: canvas.height*2-100,
+    textAlign: 'center',
+    originX: 'center',
+    fontFamily: 'Jost',
+    fontSize:54,
+    fontWeight: 'normal',
+    fill: "#111111"
+});
+canvas.add(cardPowerCanvas);
+cardPowerCanvas.selectable = false;
+
+
 
 let cardDescriptionCanvas = new fabric.Textbox('Описание карты',
 { 
-    left: 35,
-    top: 510,
+    left: 70,
+    top: 1020,
     fontFamily: 'Jost',
-    fontSize:20,
+    fontSize:40,
     fill: "#111111",
-    width:420
+    width:840
 });
 canvas.add(cardDescriptionCanvas);
 cardDescriptionCanvas.selectable = false;
@@ -76,11 +115,10 @@ function loadCardImage(e){
             cardImageSRC = new fabric.Image(imgObj);
             cardImageSRC.set({
                 angle: 0,
-                height:380,
-                width:450,
-                top: 87,
-                left: 20,
+                top: 174,
+                left: 40,
             });
+            cardImageSRC.scaleToWidth(900);
             canvas.add(cardImageSRC);
             cardImageSRC.selectable = false;
             canvas.renderAll();
@@ -106,8 +144,8 @@ function loadCardBorder(e){
                 top: 0,
                 left: 0,
             });
-            cardBorderImageSRC.scaleToHeight(770);
-            cardBorderImageSRC.scaleToWidth(490);
+            cardBorderImageSRC.scaleToHeight(1540);
+            cardBorderImageSRC.scaleToWidth(980);
             canvas.add(cardBorderImageSRC);
             canvas.sendToBack(cardBorderImageSRC)
             cardBorderImageSRC.selectable = false;
@@ -123,11 +161,11 @@ function removeSelected(){
 
 function addRect(){
     var rect = new fabric.Rect({
-        left: 100,
-        top: 100,
+        left: 200,
+        top: 200,
         fill: ShapeColor,
-        width: 20,
-        height: 20,
+        width: 200,
+        height: 200,
     });
     canvas.add(rect);
 }
@@ -143,27 +181,37 @@ function changeColor(){
 
 function addCircle(){
     var rect = new fabric.Circle({
-        left: 100,
-        top: 100,
+        left: 200,
+        top: 200,
         fill: ShapeColor,
-        radius: 20
+        radius: 200
     });
     canvas.add(rect);
 }
 
 function addText(){
     var text = new fabric.IText("Текст",{
-        left: 100,
-        top: 100,
+        left: 200,
+        top: 200,
         fill: ShapeColor,
         fontFamily: 'Jost',
-        fontSize: 20
+        fontSize: 40
     });
     canvas.add(text);
 }
 
 function nameChange(){
     cardNameCanvas.set({text:cardName.value});
+    canvas.renderAll();
+}
+
+function costChange(){
+    cardCostCanvas.set({text:cardCost.value});
+    canvas.renderAll();
+}
+
+function powerChange(){
+    cardPowerCanvas.set({text:cardPower.value});
     canvas.renderAll();
 }
 
@@ -193,7 +241,7 @@ function loadImage(){
                 top: 0,
                 left: 0,
             });
-            imageNew.scaleToWidth(390);
+            imageNew.scaleToWidth(780);
             canvas.add(imageNew);
             canvas.renderAll();
         }
